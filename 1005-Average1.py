a87cbba51c9ad5d97b3f0ceee6a0615e827daa40
OrianaCadavid <ocadavid@utp.edu.co>
2017-05-18 10:21:50 -0500

    Solved problem 1005

---
+++ 1005-Average1.py
@@ -0,0 +1,9 @@
+def main():
+    a = float(raw_input())
+    b = float(raw_input())
+    average = (a * 3.5 + b * 7.5) / 11
+    print 'MEDIA = {:.5f}'.format(average)
+
+
+if __name__ == '__main__':
+    main()

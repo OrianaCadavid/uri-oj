f88107abeb2874153bcd60b12a9b65e7075d8359
OrianaCadavid <ocadavid@utp.edu.co>
2017-05-18 10:58:25 -0500

    Solved problem 1007

---
+++ 1007-Difference.py
@@ -0,0 +1,11 @@
+def main():
+    a = int(raw_input())
+    b = int(raw_input())
+    c = int(raw_input())
+    d = int(raw_input())
+    difference = (a * b - c * d)
+    print 'DIFERENCA = {}'.format(difference)
+
+
+if __name__ == '__main__':
+    main()

64a6d3f84cb531cf49614f0047ef96a66f59d49e
OrianaCadavid <ocadavid@utp.edu.co>
2017-07-25 16:46:51 -0500

    Solved problem 1044

---
+++ 1044-Multiples.py
@@ -0,0 +1,11 @@
+def main():
+    a, b = map(int, input().split())
+
+    if a % b == 0 or b % a == 0:
+        print('Sao Multiplos')
+    else:
+        print('Nao sao Multiplos')
+
+
+if __name__ == '__main__':
+    main()
